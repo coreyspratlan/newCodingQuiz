1a4e947b2c583ffd50f1f51e25221a69e69a7ab0
Mozart <coreyspratlan@gmail.com>
2020-10-22 17:37:09 -0500

    I added the questions/choices/answers array

--- logic.js
+++ logic.js
@@ -1,17 +1,35 @@
-//setting the timer for the countdown
+// setting the time for the countdown
 var startTime = 3;
-let quizTime = startTime * 60;
-
+// setting the count down time to minutes
+var quizTime = startTime * 60;
+// setting the timer element to a variable
 var timerEl = document.getElementById("timer");
+// creating an event when the start button is clicked
+document.getElementById("start-button").addEventListener("click", function startQuiz() {
+    // setting the countdown interval to decrease by one second
+    setInterval(countDown, 1000);
+    // creating a function to countdown from the start time until zero minutes/seconds are left
+    function countDown() {
+        var minutes = Math.floor(quizTime / 60);
+        var seconds = quizTime % 60;
 
-setInterval(countDown, 1000);
-
-function countDown() {
-    var minutes = Math.floor(quizTime / 60);
-    var seconds = quizTime % 60;
-    seconds = seconds < 3 ? "0" + seconds : seconds;
-    timerEl.innerHTML = minutes + ":" + seconds;
-    quizTime--;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        timerEl.innerHTML = minutes + ":" + seconds;
+        quizTime--;
+        if (quizTime <= 0) {
+            clearInterval(quizTime = 0);
+        }
+    }
 }
+    // when the start button is clicked, I want to display the first question in the array
 
+
+
+);
+// calling the count down function once the start button has been clicked
 countDown();
+
+
+
+
+
---
+++ questions.js
@@ -0,0 +1,56 @@
+// setting up my questions array, the choices, and the correct answers.
+var questions = [
+    {
+        question: "What year was Mozart born?",
+        choices: ["1980", "1645", "1756", "1812"],
+        answer: "1756"
+    },
+    {
+        question: "What was Mozart's father's name?",
+        choices: ["Ted", "Leopold", "Larry", "Ludwig"],
+        answer: "Leopold"
+    },
+    {
+        question: "What was Mozart's sister's nickname?",
+        choices: ["Junebug", "Sweetie", "Nanneral", "Fancy"],
+        answer: "Nanneral"
+    },
+    {
+        question: "How many symphonies did Mozart compose?",
+        choices: ["101", "41", "9", "2029"],
+        answer: "41"
+    },
+    {
+        question: "Mozart was a free mason?",
+        choices: ["true", "false"],
+        answer: "true"
+    },
+    {
+        question: "Which of Mozart's operas had subtle political undetones?",
+        choices: ["The Marriage Of Figaro", "Cosi Fan Tutte", "The Magic Flute", "Don Giovanni"],
+        answer: "The Magic Flute"
+    },
+    {
+        question: "What three keys did Mozart favor?",
+        choices: ["A-flat Major, D Minor, D Major", "C Major, E-flat Major, G Minor", "G Major, E Major, A Major", "F Major, B-flat Major, D-flat Major"],
+        answer: "C Major, E-flat Major, G Minor"
+    },
+    {
+        question: "What was Mozart's wife's name?",
+        choices: ["Debbie", "Hilda", "Paula", "Constanze"],
+        answer: "Constanze"
+    },
+    {
+        question: "How many violin concertos did Mozart compose?",
+        choices: ["80", "5", "2", "none"],
+        answer: "5"
+    },
+    {
+        question: "Mozart died while composing his Requiem?",
+        choices: ["true," "false"],
+        answer: "true",
+    }]
+
+for (i = 0; i < questionArray.length; i++) {
+
+}
