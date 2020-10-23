fccf025113eb5c34821f07b22e326522e29118e3
Mozart <coreyspratlan@gmail.com>
2020-10-23 12:27:48 -0500

    The first question and the answers are displaying to the page using the askQuestions function.

--- logic.js
+++ logic.js
@@ -2,10 +2,56 @@
 var startTime = 3;
 // setting the count down time to minutes
 var quizTime = startTime * 60;
-// setting the timer element to a variable
+// setting the id's to each element
+// var welcomeScreen = document.getElementById("welcome-screen");
+var startButton = document.getElementById("start-button");
 var timerEl = document.getElementById("timer");
+var questionBox = document.getElementById("question-box");
+var question = document.getElementById("question");
+var choices = document.getElementById("choices");
+// var a = document.getElementById("a");
+// var b = document.getElementById("b");
+// var c = document.getElementById("c");
+// var d = document.getElementById("d");
+var submitButton = document.getElementById("submit-button");
+
+var questionList = 0;
+
 // creating an event when the start button is clicked
-document.getElementById("start-button").addEventListener("click", function startQuiz() {
+startButton.addEventListener("click", function startQuiz() {
+    // when the start button is clicked, I want to display the first question in the array
+    var welcomeScreen = document.getElementById("welcome-screen");
+    welcomeScreen.setAttribute("class", "hide");
+
+    questionBox.removeAttribute("class");
+
+    askQuestions();
+
+    function askQuestions() {
+
+        var questionAsked = questions[questionList];
+
+        var questionEl = document.getElementById("question");
+        questionEl.textContent = questionAsked.question;
+
+        var a = document.getElementById("A");
+        a.textContent = questionAsked.choiceA;
+
+        var b = document.getElementById("B");
+        b.textContent = questionAsked.choiceB;
+
+        var c = document.getElementById("C");
+        c.textContent = questionAsked.choiceC;
+
+        var d = document.getElementById("D");
+        d.textContent = questionAsked.choiceD;
+
+        // for (i = 0; i < questions.length; i++) {
+
+        // }
+    }
+
+
     // setting the countdown interval to decrease by one second
     setInterval(countDown, 1000);
     // creating a function to countdown from the start time until zero minutes/seconds are left
@@ -20,16 +66,10 @@ document.getElementById("start-button").addEventListener("click", function start
             clearInterval(quizTime = 0);
         }
     }
-}
-    // when the start button is clicked, I want to display the first question in the array
 
-
-
-);
+});
 // calling the count down function once the start button has been clicked
-countDown();
-
-
+// countDown();
 
 
 
--- questions.js
+++ questions.js
@@ -1,56 +1,104 @@
 // setting up my questions array, the choices, and the correct answers.
+
+
 var questions = [
     {
         question: "What year was Mozart born?",
-        choices: ["1980", "1645", "1756", "1812"],
+        choiceA: "1980",
+        choiceB: "1645",
+        choiceC: "1756",
+        choiceD: "1812",
         answer: "1756"
     },
     {
         question: "What was Mozart's father's name?",
-        choices: ["Ted", "Leopold", "Larry", "Ludwig"],
+        choiceA: "Ted",
+        choiceB: "Leopold",
+        choiceC: "Larry",
+        choiceD: "Ludwig",
         answer: "Leopold"
-    },
-    {
-        question: "What was Mozart's sister's nickname?",
-        choices: ["Junebug", "Sweetie", "Nanneral", "Fancy"],
-        answer: "Nanneral"
-    },
-    {
-        question: "How many symphonies did Mozart compose?",
-        choices: ["101", "41", "9", "2029"],
-        answer: "41"
-    },
-    {
-        question: "Mozart was a free mason?",
-        choices: ["true", "false"],
-        answer: "true"
-    },
-    {
-        question: "Which of Mozart's operas had subtle political undetones?",
-        choices: ["The Marriage Of Figaro", "Cosi Fan Tutte", "The Magic Flute", "Don Giovanni"],
-        answer: "The Magic Flute"
-    },
-    {
-        question: "What three keys did Mozart favor?",
-        choices: ["A-flat Major, D Minor, D Major", "C Major, E-flat Major, G Minor", "G Major, E Major, A Major", "F Major, B-flat Major, D-flat Major"],
-        answer: "C Major, E-flat Major, G Minor"
-    },
-    {
-        question: "What was Mozart's wife's name?",
-        choices: ["Debbie", "Hilda", "Paula", "Constanze"],
-        answer: "Constanze"
-    },
-    {
-        question: "How many violin concertos did Mozart compose?",
-        choices: ["80", "5", "2", "none"],
-        answer: "5"
-    },
-    {
-        question: "Mozart died while composing his Requiem?",
-        choices: ["true," "false"],
-        answer: "true",
-    }]
+    }
+]
 
-for (i = 0; i < questionArray.length; i++) {
 
-}
+// var questions = [
+//     {
+//         question: "What year was Mozart born?",
+//         choices: ["1980", "1645", "1756", "1812"],
+//         answer: "1756"
+//     },
+//     {
+//         question: "What was Mozart's father's name?",
+//         choices: ["Ted", "Leopold", "Larry", "Ludwig"],
+//         answer: "Leopold"
+//     }
+    // ,
+    // {
+    //     question: "What was Mozart's sister's nickname?",
+    //     choices: ["Junebug", "Sweetie", "Nanneral", "Fancy"],
+    //     answer: "Nanneral"
+    // },
+    // {
+    //     question: "How many symphonies did Mozart compose?",
+    //     choices: ["101", "41", "9", "2029"],
+    //     answer: "41"
+    // },
+    // {
+    //     question: "Mozart was a free mason?",
+    //     choices: ["true", "false"],
+    //     answer: "true"
+    // },
+    // {
+    //     question: "Which of Mozart's operas had subtle political undetones?",
+    //     choices: ["The Marriage Of Figaro", "Cosi Fan Tutte", "The Magic Flute", "Don Giovanni"],
+    //     answer: "The Magic Flute"
+    // },
+    // {
+    //     question: "What three keys did Mozart favor?",
+    //     choices: ["A-flat Major, D Minor, D Major", "C Major, E-flat Major, G Minor", "G Major, E Major, A Major", "F Major, B-flat Major, D-flat Major"],
+    //     answer: "C Major, E-flat Major, G Minor"
+    // },
+    // {
+    //     question: "What was Mozart's wife's name?",
+    //     choices: ["Debbie", "Hilda", "Paula", "Constanze"],
+    //     answer: "Constanze"
+    // },
+    // {
+    //     question: "How many violin concertos did Mozart compose?",
+    //     choices: ["80", "5", "2", "none"],
+    //     answer: "5"
+    // },
+    // {
+    //     question: "Mozart died while composing his Requiem?",
+    //     choices: ["true,", "false"],
+    //     answer: "true"
+    // }]
+
+
+
+// var questions = [
+//     "What year was Mozart born?", 
+//     "What was Mozart's father's name?",
+//     "What was Mozart's sister's nickname?",
+//     "How many symphonies did Mozart compose?",
+//     "Mozart was a free mason?",
+//     "Which of Mozart's operas had subtle political undetones?",
+//     "What three keys did Mozart favor?",
+//     "What was Mozart's wife's name?",
+//     "How many violin concertos did Mozart compose?",
+//     "Mozart died while composing his Requiem?"
+// ]
+
+// var choices = [
+//     ["1980","1645","1756","1812"],
+//     ["Ted", "Leopold", "Larry", "Ludwig"],
+//     ["Junebug", "Sweetie", "Nanneral", "Fancy"],
+//     ["101", "41", "9", "2029"],
+//     ["true", "false"],
+//     ["The Marriage Of Figaro", "Cosi Fan Tutte", "The Magic Flute", "Don Giovanni"],
+//     ["A-flat Major, D Minor, D Major", "C Major, E-flat Major, G Minor" , "G Major, E Major, A Major" , "F Major, B-flat Major, D-flat Major"],
+//     ["Debbie", "Hilda", "Paula", "Constanze"],
+//     ["80", "5", "2", "none"],
+//     ["true", "false"]
+
+// ]
